9971f7e9aef3c1bb0d0972a072982b6dcfc0c4f4
ellenoireQ <ketchupnaiso@gmail.com>
2025-10-01 10:01:11 +0700

    APi: handle path "/" then just print to UI

--- main.go
+++ main.go
@@ -8,59 +8,60 @@ import (
 
 // Body of JSON
 type body struct {
-	UID string `json:"uid"`
-	Messages string	`json:"messages"`
-	Lastime  string	`json:"lastime"`
+	UID      string `json:"uid"`
+	Messages string `json:"messages"`
+	Lastime  string `json:"lastime"`
 }
 
 // Conversation JSON
 type Conversation struct {
 	RoomID string `json:"room_id"`
-	Users []body `json:"users"`
+	Users  []body `json:"users"`
 }
 
 // Chat definition
 // Maybe looks like
-//	{
-//    "conversation_id": "abc123",
-//    "body": {
-//      "room_id": "room001",
-//      "users": [
-//        {
-//          "uid": "user001",
-//          "messages": "Hello World",
-//          "lastime": "23 Aug 2025"
-//        },
-//        {
-//          "uid": "user002",
-//          "messages": "Hi back!",
-//          "lastime": "23 Aug 2025"
-//        }
-//      ]
-//    }
-//  }
+//
+//		{
+//	   "conversation_id": "abc123",
+//	   "body": {
+//	     "room_id": "room001",
+//	     "users": [
+//	       {
+//	         "uid": "user001",
+//	         "messages": "Hello World",
+//	         "lastime": "23 Aug 2025"
+//	       },
+//	       {
+//	         "uid": "user002",
+//	         "messages": "Hi back!",
+//	         "lastime": "23 Aug 2025"
+//	       }
+//	     ]
+//	   }
+//	 }
 type ChatDefinition struct {
-	Conversation_id string `json:"conversation_id"`
-	Body Conversation `json:"body"`
+	Conversation_id string       `json:"conversation_id"`
+	Body            Conversation `json:"body"`
 }
 
-
-//
 // Main function right here
-//
 func main() {
 	databaseChat := []ChatDefinition{}
 	router := gin.Default()
-	router.POST("/sendMess", func(c *gin.Context){
+	router.POST("/sendMess", func(c *gin.Context) {
 		var newChat ChatDefinition
 
-		if err := c.BindJSON(&newChat); err != nil{
+		if err := c.BindJSON(&newChat); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Request"})
 		}
 		databaseChat = append(databaseChat, newChat)
 	})
-	router.GET("/get", func(c *gin.Context){
+	router.GET("/get", func(c *gin.Context) {
 		c.JSON(http.StatusOK, databaseChat)
+	})
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Welcome to the TinyGoChat API v1.0")
 	})
 	router.Run(":8080")
 }
